03dae832ffb782a416a0577d07a86b900efaa04f
Kosuke_Zaizen <kosukezaizen@yahoo.co.jp>
2021-11-03 19:09:37 +0900

    make response type more strict

--- pages/api/zApps/folktales/getOneSentence.ts
+++ pages/api/zApps/folktales/getOneSentence.ts
@@ -1,5 +1,6 @@
 import { fetchZApps } from "../../../../lib/fetch";
 import { makeApi } from "../../../../lib/nextApi";
+import { StrictQuery } from "../../../../types/next";
 import { sentence, word } from "../../../../types/stories";
 
 export interface GetOneSentenceQuery {
@@ -12,12 +13,14 @@ export interface GetOneSentenceResponse {
     words: word[];
 }
 
-export default makeApi<GetOneSentenceQuery, GetOneSentenceResponse>(
-    async query => {
-        const { storyName, lineNumber } = query;
-        const url = `api/Stories/GetOneSentence/${storyName}/${lineNumber}`;
-        const response = await fetchZApps(url);
-        const { sentence, words } = await response.json();
-        return { sentence, words };
-    }
-);
+const handler = async (
+    query: StrictQuery<GetOneSentenceQuery>
+): Promise<GetOneSentenceResponse> => {
+    const { storyName, lineNumber } = query;
+    const url = `api/Stories/GetOneSentence/${storyName}/${lineNumber}`;
+    const response = await fetchZApps(url);
+    const { sentence, words } = await response.json();
+    return { sentence, words };
+};
+
+export default makeApi(handler);
