aaa355e2dcda7f97262dd8e028373005fced14be
Kosuke_Zaizen <kosukezaizen@yahoo.co.jp>
2021-11-19 10:38:06 +0900

    quit to use next image

--- components/articles/SnsShareButton.tsx
+++ components/articles/SnsShareButton.tsx
@@ -1,29 +1,25 @@
 import { SerializedStyles } from "@emotion/utils";
 import * as React from "react";
 import { BLOB_URL } from "../../const/public";
-import { Img } from "../shared/Img";
 import { ATargetBlank } from "../shared/Link/ATargetBlank";
 
 type TFBProps = {
     style: SerializedStyles;
     urlToShare: string;
 };
-export const FBShareBtn = ({ style, urlToShare }: TFBProps) => {
+export const FBShareBtn = (props: TFBProps) => {
+    const { style, urlToShare } = props;
+
     return (
         <ATargetBlank
             href={`https://www.facebook.com/share.php?u=${urlToShare}`}
             nofollow
         >
-            <div css={style}>
-                <Img
-                    src={BLOB_URL + "/vocabulary-quiz/img/shareOnFacebook.png"}
-                    alt="Share on Facebook"
-                    layout="intrinsic"
-                    width={200}
-                    height={32}
-                    loading="eager"
-                />
-            </div>
+            <img
+                src={BLOB_URL + "/vocabulary-quiz/img/shareOnFacebook.png"}
+                alt="Share on Facebook"
+                css={style}
+            />
         </ATargetBlank>
     );
 };
@@ -41,16 +37,11 @@ export const TwitterShareBtn = (props: TTWProps) => {
             href={`https://twitter.com/share?url=${urlToShare}&text=${textToShare}&hashtags=nihongo,Japanese,LingualNinja`}
             nofollow
         >
-            <div css={style}>
-                <Img
-                    src={BLOB_URL + "/vocabulary-quiz/img/shareOnTwitter.png"}
-                    alt="Share on Twitter"
-                    layout="intrinsic"
-                    width={200}
-                    height={32}
-                    loading="eager"
-                />
-            </div>
+            <img
+                src={BLOB_URL + "/vocabulary-quiz/img/shareOnTwitter.png"}
+                alt="Share on Twitter"
+                css={style}
+            />
         </ATargetBlank>
     );
 };
