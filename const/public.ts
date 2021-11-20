45fe56f01da94d19e9121961fc6e07d2fe8c57b9
Kosuke_Zaizen <kosukezaizen@yahoo.co.jp>
2021-11-20 15:07:59 +0900

    make sitemap for articles

--- const/public.ts
+++ const/public.ts
@@ -1,3 +1,24 @@
+// List of app names
+export const appName = { articles: "articles" } as const;
+
+const appNames = Object.keys(appName) as (keyof typeof appName)[];
+
+// url info of each app
+export const apps = appNames.reduce(
+    (acc, name) => ({
+        ...acc,
+        [name]: {
+            host: `${name}.lingual-ninja.com`,
+            url: `https://${name}.lingual-ninja.com`,
+        },
+    }),
+    {} as Apps
+);
+export type Apps = {
+    [key in keyof typeof appName]: { host: string; url: string };
+};
+
+// Z-Apps info
 export const Z_APPS_HOST = "www.lingual-ninja.com";
 export const Z_APPS_TOP_URL = `https://${Z_APPS_HOST}`;
 
---
+++ pages/articles/sitemap.xml.tsx
@@ -0,0 +1,42 @@
+import { GetServerSidePropsContext } from "next";
+import { apps } from "../../const/public";
+import { fetchZApps } from "../../lib/fetch";
+import { Page } from "./[pageName]";
+
+const topUrl = apps.articles.url;
+
+export const getServerSideProps = async ({
+    res,
+}: GetServerSidePropsContext) => {
+    const xml = await generateSitemapXml();
+
+    res.statusCode = 200;
+    res.setHeader("Cache-Control", "s-maxage=86400, stale-while-revalidate"); // Cache for 24 hours
+    res.setHeader("Content-Type", "text/xml");
+    res.end(xml);
+
+    return {
+        props: {},
+    };
+};
+
+const SitemapPage = () => null;
+export default SitemapPage;
+
+async function generateSitemapXml(): Promise<string> {
+    let xml = `<?xml version="1.0" encoding="UTF-8"?><urlset xmlns="http://www.sitemaps.org/schemas/sitemap/0.9">`;
+
+    const response: Response = await fetchZApps("api/Articles/GetAllArticles");
+    const pages: Page[] = await response.json();
+    const paths = pages
+        .map(p => p.url?.toLowerCase())
+        .filter(u => u)
+        .map(u => `${topUrl}/articles/${u}`);
+
+    [topUrl, ...paths].forEach(url => {
+        xml += `<url><loc>${url}</loc></url>`;
+    });
+
+    xml += `</urlset>`;
+    return xml;
+}
