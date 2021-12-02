426add720b2e47ba7c766f497568e0f120627f1a
Kosuke_Zaizen <kosukezaizen@yahoo.co.jp>
2021-12-02 14:34:04 +0900

    remove sitemap cache

--- pages/articles/sitemap.xml.tsx
+++ pages/articles/sitemap.xml.tsx
@@ -14,7 +14,7 @@ export const getServerSideProps = async ({
     const xml = await generateSitemapXml();
 
     res.statusCode = 200;
-    res.setHeader("Cache-Control", "s-maxage=86400, stale-while-revalidate"); // Cache for 24 hours
+    // res.setHeader("Cache-Control", "s-maxage=86400, stale-while-revalidate"); // Cache for 24 hours
     res.setHeader("Content-Type", "text/xml");
     res.end(xml);
 
