80d2507fb1400a11bbe71996eba590d0e6f107a7
Kosuke_Zaizen <kosukezaizen@yahoo.co.jp>
2022-01-31 20:45:07 +0900

    remove each path

--- pages/api/next/removeStaticCache.ts
+++ pages/api/next/removeStaticCache.ts
@@ -1,20 +1,19 @@
 import { apiGet } from "../../../lib/nextApi";
-import { EmptyObject } from "../../../types/util";
 import child_process from "child_process";
 import { getErrorMessage } from "../../../lib/error";
 import { execCommandAsync } from "../../../lib/childProcess";
-import { generateSitemapXml } from "../../articles/sitemap.xml";
 import { apps } from "../../../const/public";
+import { GetParams } from "../../../types/next";
+import { generateSitemapXml } from "../../articles/sitemap.xml";
 
 export interface RemoveStaticCache {
     url: "/api/next/removeStaticCache";
     params: Params;
     response: Response;
 }
-type Params = EmptyObject;
+type Params = { path: string };
 type Response = {
     message: string;
-    lastExecutionDate: string;
     executionResults?: ExecutionResults;
 };
 type ExecutionResult = {
@@ -23,79 +22,62 @@ type ExecutionResult = {
     error: child_process.ExecException | null;
 };
 type ExecutionResults = {
-    [url: string]: {
-        htmlDeletion: ExecutionResult;
-        jsonDeletion: ExecutionResult;
-        fetchStatus: number;
-    };
+    htmlDeletion: ExecutionResult;
+    jsonDeletion: ExecutionResult;
+    fetchStatus: number;
 };
-let lastResponse: Response | null = null;
-let isExecutingNow = false;
 
-const handler = async (): Promise<Response> => {
-    const date = new Date();
-    const year = date.getFullYear();
-    const month = date.getMonth() + 1;
-    const day = date.getDate();
-
-    const today = `${year}/${month}/${day}`;
-
-    if (isExecutingNow) {
-        return {
-            message:
-                "Now another person is executing the same command! Wait for a while!",
-            lastExecutionDate: `${year}/${month}/${day}`,
-            executionResults: lastResponse?.executionResults,
-        };
-    }
-
-    if (today === lastResponse?.lastExecutionDate) {
-        return {
-            message: "Today's execution has already been finished!",
-            lastExecutionDate: `${year}/${month}/${day}`,
-            executionResults: lastResponse.executionResults,
-        };
-    }
-
-    isExecutingNow = true;
+const handler = async ({ path }: GetParams<Params>): Promise<Response> => {
     try {
-        const sitemap = await generateSitemapXml();
-        const urls = sitemap
-            .split("<loc>")
-            .filter((u, i) => i)
-            .map(u => u.split("</loc>")[0])
-            .map(u => u.replace(apps.articles.url, ""));
-
-        const executionResults: ExecutionResults = {};
-        for (const url of urls) {
-            const commandPath = url.split("/").join("\\");
-
-            executionResults[url] = {
-                htmlDeletion: await execCommandAsync(
-                    `del .\\.next\\server\\pages${commandPath}.html`
-                ),
-                jsonDeletion: await execCommandAsync(
-                    `del .\\.next\\server\\pages${commandPath}.json`
-                ),
-                fetchStatus: (await fetch(`${apps.articles.url}${url}`)).status,
+        if (typeof path !== "string") {
+            return {
+                message: "Error! Invalid parameter.",
             };
         }
 
-        lastResponse = {
+        // Prevent from being attacked
+        const isValidPath = await checkValidPath(path);
+        if (!isValidPath) {
+            return {
+                message: "Error! Invalid parameter.",
+            };
+        }
+
+        const commandPath = path.split("/").join("\\");
+
+        const htmlDeletion = await execCommandAsync(
+            `del .\\.next\\server\\pages${commandPath}.html`
+        );
+        const jsonDeletion = await execCommandAsync(
+            `del .\\.next\\server\\pages${commandPath}.json`
+        );
+
+        const fetchStatus = (await fetch(`${apps.articles.url}${path}`)).status;
+
+        return {
             message: "Done!",
-            lastExecutionDate: `${year}/${month}/${day}`,
-            executionResults,
+            executionResults: {
+                htmlDeletion,
+                jsonDeletion,
+                fetchStatus,
+            },
         };
-        isExecutingNow = false;
-        return lastResponse;
     } catch (error) {
-        lastResponse = {
+        return {
             message: "Error! " + getErrorMessage(error),
-            lastExecutionDate: `${year}/${month}/${day}`,
         };
-        isExecutingNow = false;
-        return lastResponse;
     }
 };
+
+async function checkValidPath(path: string) {
+    const sitemap = await generateSitemapXml();
+    const urls = sitemap
+        .split("<loc>")
+        .filter((u, i) => i)
+        .map(u => u.split("</loc>")[0])
+        .map(u => u.replace(apps.articles.url, ""));
+
+    return urls.includes(path);
+}
 
 export default apiGet<RemoveStaticCache>(handler);
